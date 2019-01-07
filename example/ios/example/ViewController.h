e7441d55ec186e1dc9b814971d994cfcaeee8810
xiewei <xiewei@bytedance.com>
2019-01-07 18:00:52 +0800

    init project
    Change-Id: Iab395bdea9a658aa9f54ab177a9e576827382306

---
+++ example/ios/example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  example
+//
+//  Created by xiewei on 2019/1/7.
+//  Copyright © 2019 xiewei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
